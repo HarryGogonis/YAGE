fd0be8bb8e57e3b14a596351ae2f7b4e84536200
Harry Gogonis <hgogonis@gmail.com>
2016-01-24 00:01:34 -0500

    Update Vector3 class with various operations (for example: scalar/vector multiplication and addition)

--- OpenGL_01/Rendering/Util/Vector3.cpp
+++ OpenGL_01/Rendering/Util/Vector3.cpp
@@ -1,5 +1,7 @@
 #include "Vector3.h"
 
+const float Vector3::SCALE = 0.01f;
+
 Vector3::Vector3()
 {
 	this->x = 0.0f;
@@ -32,8 +34,39 @@ void Vector3::Set(float x, float y, float z)
 	this->z = z;
 }
 
+const glm::vec3 Vector3::toVec3() const
+{
+	return Vector3::SCALE * glm::vec3(x,y,z);
+}
+
+Vector3 operator*(Vector3 v1, Vector3 v2)
+{
+	//TODO implement vector multiplication
+	return Vector3(v1.toVec3() * v2.toVec3());
+}
+
+Vector3 operator+(Vector3 v1, Vector3 v2)
+{
+	return Vector3(
+			v1.x + v2.x,
+			v1.y + v2.y,
+			v1.z + v2.z);
+}
+
+Vector3 operator*(float c, Vector3 v)
+{
+	return Vector3(c * v.x, c * v.y, c * v.z);
+}
+
+bool operator==(Vector3 v1, Vector3 v2)
+{
+	return (v1.x == v2.x) &&
+		(v1.y == v2.y) &&
+		(v1.z == v2.z);
+}
+
 std::ostream & operator<<(std::ostream & os, const Vector3 & v)
 {
-	os << "x:" << v.x << "\ty:" << v.x << "\tz:" << v.z << std::endl;
+	os << "x:" << v.x << "\ty:" << v.y << "\tz:" << v.z << std::endl;
 	return os;
 }
--- OpenGL_01/Rendering/Util/Vector3.h
+++ OpenGL_01/Rendering/Util/Vector3.h
@@ -7,12 +7,22 @@
 class Vector3
 {
 public:
+	static const float SCALE; // [0,100] => [0,1]
+
 	Vector3();
 	Vector3(float, float, float);
 	Vector3(const glm::vec3&);
 	~Vector3();
 	void Set(float, float, float);
+
+	const glm::vec3 toVec3() const;
+
 	float x, z, y;
+
+	friend Vector3 operator* (Vector3, Vector3);
+	friend Vector3 operator+ (Vector3, Vector3);
+	friend Vector3 operator* (float, Vector3);
+	friend bool operator== (Vector3, Vector3);
 
 	friend std::ostream& operator<<(std::ostream& os, const Vector3& v);
 };
