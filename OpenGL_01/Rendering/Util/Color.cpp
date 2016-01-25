d108c1e28f1f7d9f1ea6edec36c7675d7b89e709
Harry Gogonis <hgogonis@gmail.com>
2016-01-24 19:42:22 -0500

    Implement all the static Colors

--- OpenGL_01/Rendering/Util/Color.cpp
+++ OpenGL_01/Rendering/Util/Color.cpp
@@ -3,9 +3,19 @@
 /*
  * Static parameters
  */
-const Color Color::RED		= Color(1.0, 0.0, 0.0, 1.0);
-const Color Color::GREEN	= Color(0.0, 1.0, 0.0, 1.0);
-const Color Color::BLUE		= Color(0.0, 0.0, 1.0, 1.0);
+const Color Color::BLACK		= Color(0.0, 0.0, 0.0);
+const Color Color::BLUE			= Color(0.0, 0.0, 1.0);
+const Color Color::CYAN			= Color(0.0, 1.0, 1.0);
+const Color Color::DARK_GRAY	= Color(0.25, 0.25, 0.25);
+const Color Color::GRAY			= Color(0.5, 0.5, 0.5);
+const Color Color::GREEN		= Color(0.0, 1.0, 0.0);
+const Color Color::LIGHT_GRAY	= Color(0.75, 0.75, 0.75);
+const Color Color::MAGENTA		= Color(1.0, 0.0, 1.0);
+const Color Color::ORANGE		= Color(1.0, 0.80, 0.0);
+const Color Color::PINK			= Color(1.0, 0.5, 0.5);
+const Color Color::RED			= Color(1.0, 0.0, 0.0);
+const Color Color::WHITE		= Color(1.0, 1.0, 1.0);
+const Color Color::YELLOW		= Color(1.0, 1.0, 0.0);
 
 /*
  * Constructor
