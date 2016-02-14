2e6c0666a96c2607646f29694fe5d846c2bfac87
Harry Gogonis <hgogonis@gmail.com>
2016-02-14 14:15:27 -0500

    Add method that reads in textures from BMP file and sends them to OpenGL

--- OpenGL_01/Managers/Model_Manager.cpp
+++ OpenGL_01/Managers/Model_Manager.cpp
@@ -52,6 +52,75 @@ const IGameObject& Models_Manager::GetModel(const std::string& gameModelName)
 	return (*gameModelList.at(gameModelName));
 }
 
+/*
+ * Read in a BMP texture file
+ */
+GLuint Models_Manager::LoadTexture(const char * filename)
+{
+	/* 
+	 * http://stackoverflow.com/a/12524013/3134975
+	 * http://www.opengl-tutorial.org/beginners-tutorials/tutorial-5-a-textured-cube/
+	*/
+	GLuint texture;
+
+	unsigned char * data;
+	unsigned int dataPos;
+	unsigned int width, height;
+	unsigned int imageSize;
+	unsigned char header[54];
+
+	// Open File
+	FILE * file = fopen(filename, "rb");
+	if (file == NULL)
+	{
+		std::cerr << "Error opening image: " << filename << std::endl;
+		return 0;
+	}
+	// Check file headers
+	if (fread(header, 1, 54, file) != 54)
+	{
+		std::cerr << "Invalid BMP file: " << filename << std::endl;
+		return 0;
+	}
+	if (header[0] != 'B' || header[1] != 'M')
+	{
+		std::cerr << "Invalid BMP file: " << filename << std::endl;
+		return 0;
+	}
+
+	// Setup image parameters
+	dataPos = *(int*)&(header[0x0A]);
+	imageSize = *(int*)&(header[0x22]);
+	width = *(int*)&(header[0x12]);
+	height = *(int*)&(header[0x16]);
+	if (imageSize == 0) imageSize = width * height * 3;
+	if (dataPos == 0)	dataPos = 54;
+
+	// Create a buffer for data
+	data = new unsigned char[imageSize];
+
+	// Read data from file to buffer
+	fread(data, 1, imageSize, file); 
+	fclose(file);
+
+	// Pass data to OpenGL
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
+
+	// Clear buffer
+	delete[] data;
+
+	// Trilinear filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	return texture;
+}
+
 void Models_Manager::Update()
 {
 	// iterate through model list and Update
--- OpenGL_01/Managers/Models_Manager.h
+++ OpenGL_01/Managers/Models_Manager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <map>
+#include <iostream>
 #include "Shader_Manager.h"
 #include "../Rendering/IGameObject.h"
 #include "../Rendering/Models/Triangle.h"
@@ -16,6 +17,12 @@ public:
 	//void CreateModel(const std::string& gameModelName);
 	void DeleteModel(const std::string& gameModelName);
 	const IGameObject& GetModel(const std::string& gameModelName);
+
+	bool LoadObject(const char* path,
+		std::vector<glm::vec3>& out_vertices,
+		std::vector<glm::vec2>& out_uvs,
+		std::vector<glm::vec3>& out_normals);
+	GLuint LoadTexture(const char* path);
 
 private:
 	// for big games, use a vector instead of a map
